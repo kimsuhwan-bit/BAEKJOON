ae847e339b0bf98ec1c5955bdce5dc7fa6095f1f
suhwan <suhwan079@gmail.com>
2021-03-11 20:31:37 +0900

    No.2557

--- BAEKJOON/src/step1/Hello_World.java
+++ BAEKJOON/src/step1/Hello_World.java
@@ -8,3 +8,11 @@ public class Hello_World {
 	}
 
 }
+
+// 2021-03-11 (목)
+// 채점 시 class 이름을 Main으로 변경한 후에 제출해야 된다.
+
+// [Rule] 
+// Class 이름은 문제의 제목으로 하고 공백은 _로 대체한다.
+// Package 이름은 단계를 step으로 대체한다.
+// Git에 commit시 comment는 No.문제 번호로 한다.

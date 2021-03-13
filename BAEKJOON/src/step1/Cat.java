32ac46415c2e5a9d94e277524219f9974916a919
suhwan <suhwan079@gmail.com>
2021-03-13 14:34:33 +0900

    No.10171

---
+++ BAEKJOON/src/step1/Cat.java
@@ -0,0 +1,27 @@
+package step1;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		
+		System.out.println('\\' + "    " +  "/\\");
+		System.out.println(" " + ')' + "  " + '(' + " ')");
+		System.out.println("(" + "  /" + "  )");
+		System.out.println(" \\(__)|");
+		
+	}
+
+}
+
+// BEAKJOON에 제출할 때 package를 빼고 제출해야한다!
+
+// 	BEAKJOON 제출 형식! 
+
+//	public class Main {
+//		public static void main(String[] args) {
+//				
+// 		}
+// 	}
+// 
+
+// 고양이를 출력해본건 처음인데 단순하고 귀엽다!

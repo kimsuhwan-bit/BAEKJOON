ed30690cdbc8e5fe1bddee11cb82691455fdcd10
suhwan <suhwan079@gmail.com>
2021-03-13 14:34:15 +0900

    No.10172

---
+++ BAEKJOON/src/step1/Dog.java
@@ -0,0 +1,17 @@
+package step1;
+
+public class Dog {
+
+	public static void main(String[] args) {
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|" + "   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`" + "    |");
+		System.out.println("||_/=\\\\__|");
+	}
+
+}
+
+// 고양이 문제를 풀고 바로 푸니까 아~~주 간단하다!!
+// 좀 귀여운 문제인듯!! ㅎㅎ

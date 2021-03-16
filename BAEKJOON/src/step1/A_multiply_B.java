ea98f5cfa38cdfe6a7f382588708b6ada1ba58af
suhwan <suhwan079@gmail.com>
2021-03-16 15:30:45 +0900

    No.10998

---
+++ BAEKJOON/src/step1/A_multiply_B.java
@@ -0,0 +1,20 @@
+package step1;
+
+import java.util.Scanner;
+
+public class A_multiply_B {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a * b);
+	}
+
+}
+
+// 문제번호 : 10998
+// 메모리 : 18324KB 
+// 시간 : 232ms

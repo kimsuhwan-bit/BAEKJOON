f2056c6c9fb8112958aaa8877f327cacdefb1d7f
suhwan <suhwan079@gmail.com>
2021-03-16 15:29:57 +0900

    No.1008

---
+++ BAEKJOON/src/step1/A_div_B.java
@@ -0,0 +1,20 @@
+package step1;
+
+import java.util.Scanner;
+
+public class A_div_B {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		
+		System.out.println(a / b);
+	}
+
+}
+
+// 문제 번호 : 1008
+// 메모리 : 18408KB 
+// 시간 : 228ms

927be444918ea65447a16582b32ecff433de36a9
suhwan <suhwan079@gmail.com>
2021-03-17 19:09:02 +0900

    No.10869

---
+++ BAEKJOON/src/step1/Four_rule_calculations.java
@@ -0,0 +1,24 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Four_rule_calculations {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+
+}
+
+// 문제번호 : 10869
+// 메모리 : 18396KB
+// 시간 : 224ms

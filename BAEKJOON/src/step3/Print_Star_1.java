7bd5c9055eac8c5f71c86818bded079178b19fc8
suhwan <suhwan079@gmail.com>
2021-03-28 16:34:41 +0900

    No.2438

---
+++ BAEKJOON/src/step3/Print_Star_1.java
@@ -0,0 +1,34 @@
+package step3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Print_Star_1 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=0; j<i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
+
+// 문제 번호 : 2438
+// 메모리 : 14604KB
+// 시간 : 152ms
